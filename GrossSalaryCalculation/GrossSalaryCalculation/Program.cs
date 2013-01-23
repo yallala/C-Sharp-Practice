924988b2a59e85705733573790f9fd9dad71cb64
Sudheer <yc.sudheerkumar@gmail.com>
2013-01-23 16:39:41 -0500

    Initial Project Info
    Initial Project Info....

---
+++ GrossSalaryCalculation/GrossSalaryCalculation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrossSalaryCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+        }
+    }
+}
